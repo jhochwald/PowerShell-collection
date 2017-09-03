6cc5178f7029da97c1a5b4f110cd13c7f532baed
Joerg Hochwald <joerg@hochwald.net>
2017-09-03 17:41:16 +0200

    Initial Commit of the S4B QoS auutomation

---
+++ Skype_for_Business_QoS/New-QoS-SkypeForBusinessClient_GPO.ps1
@@ -0,0 +1,229 @@
+﻿#Requires -RunAsAdministrator
+
+<#
+		.SYNOPSIS
+		DSCP markings for Lync/Skype for Business client traffic.
+
+		.DESCRIPTION
+		Creates a new GPO for DSCP markings for Lync/Skype for Business client traffic.
+
+		.EXAMPLE
+		PS C:\> .\New-QoS-SkypeForBusinessClient_GPO.ps1
+
+		# Creates a new GPO for DSCP markings for Lync/Skype for Business client traffic.
+
+		.NOTES
+		Copyright (c) 2017 Joerg Hochwald (http://jhochwald.com). All rights reserved.
+
+		Redistribution and use in source and binary forms, with or without modification,
+		are permitted provided that the following conditions are met:
+
+		1.	Redistributions of source code must retain the above copyright notice,
+		this list of conditions and the following disclaimer.
+
+		2.	Redistributions in binary form must reproduce the above copyright notice, this list of
+		conditions and the following disclaimer in the documentation and/or other materials
+		provided with the distribution.
+
+		3.	Neither the name of the copyright holder nor the names of its contributors may be used
+		to endorse or promote products derived from this software without specific prior
+		written permission.
+
+		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+		IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+		AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+		CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+		CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+		SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+		THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+		OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+		POSSIBILITY OF SUCH DAMAGE.
+
+		By using the Software, you agree to the License, Terms and Conditions above!
+#>
+[CmdletBinding()]
+param ()
+
+#region Variables
+$GPOName = 'QoS - Skype for Business Client'
+$SC = 'SilentlyContinue'
+$STP = 'Stop'
+#endregion Variables
+
+#region LoadActiveDirectoryModule
+try
+{
+	$paramImportModule = @{
+		Name          = 'ActiveDirectory'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Import-Module @paramImportModule)
+}
+catch
+{
+	Write-Error -Message 'ActiveDirectory PowerShell Module not found...' -ErrorAction $STP
+}
+#endregion LoadActiveDirectoryModule
+
+#region LoadGroupPolicyModule
+try
+{
+	$paramImportModule = @{
+		Name          = 'GroupPolicy'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Import-Module @paramImportModule)
+}
+catch
+{
+	Write-Error -Message 'GroupPolicy PowerShell Module not found...' -ErrorAction $STP
+}
+#endregion LoadGroupPolicyModule
+
+#region CreateThePolicy
+try
+{
+	$paramNewGPO = @{
+		Name          = $GPOName
+		Comment       = 'DSCP markings for Lync/Skype for Business client traffic.'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (New-GPO @paramNewGPO)
+
+	$paramGetGPO = @{
+		Name          = $GPOName
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = ((Get-GPO @paramGetGPO).GpoStatus = 'UserSettingsDisabled')
+}
+catch
+{
+	Write-Error "Unable to create $GPOName" -ErrorAction Stop
+}
+#endregion CreateThePolicy
+
+#region DoNotUseNLA
+try
+{
+	$paramSetGPPrefRegistryValue = @{
+		Name          = $GPOName
+		Context       = 'Computer'
+		Key           = 'HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\QoS'
+		ValueName     = 'Do not use NLA'
+		Value         = '1'
+		Type          = 'String'
+		Action        = 'Update'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Set-GPPrefRegistryValue @paramSetGPPrefRegistryValue)
+}
+catch
+{
+	Write-Warning 'Unable to create Do Not Use NLA'
+}
+#endregion DoNotUseNLA
+
+#region SIPSignallingQoS
+try
+{
+	$paramSetGPRegistryValue = @{
+		Name          = $GPOName
+		Key           = 'HKLM\SOFTWARE\Policies\Microsoft\Windows\QoS\Skype for Business Client SIP Signaling QoS'
+		ValueName     = 'Version', 'Application Name', 'Protocol', 'Local Port', 'Local IP', 'Local IP Prefix Length', 'Remote Port', 'Remote IP', 'Remote IP Prefix Length', 'DSCP Value', 'Throttle Rate'
+		Type          = 'String'
+		Value         = '1.0', '*', '*', '5060:5069', '*', '*', '*', '*', '*', '40', '-1'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Set-GPRegistryValue @paramSetGPRegistryValue)
+}
+catch
+{
+	Write-Warning 'Unable to create SIP Signaling QoS'
+}
+#endregion SIPSignallingQoS
+
+#region ClientAudioQoS
+try
+{
+	$paramSetGPRegistryValue = @{
+		Name          = $GPOName
+		Key           = 'HKLM\SOFTWARE\Policies\Microsoft\Windows\QoS\Skype for Business Client Audio QoS'
+		ValueName     = 'Version', 'Application Name', 'Protocol', 'Local Port', 'Local IP', 'Local IP Prefix Length', 'Remote Port', 'Remote IP', 'Remote IP Prefix Length', 'DSCP Value', 'Throttle Rate'
+		Type          = 'String'
+		Value         = '1.0', '*', '*', '50020:50039', '*', '*', '*', '*', '*', '46', '-1'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Set-GPRegistryValue @paramSetGPRegistryValue)
+}
+catch
+{
+	Write-Warning 'Unable to create Client Audio QoS'
+}
+#endregion ClientAudioQoS
+
+#region ClientVideoQoS
+try
+{
+	$paramSetGPRegistryValue = @{
+		Name          = $GPOName
+		Key           = 'HKLM\SOFTWARE\Policies\Microsoft\Windows\QoS\Skype for Business Client Video QoS'
+		ValueName     = 'Version', 'Application Name', 'Protocol', 'Local Port', 'Local IP', 'Local IP Prefix Length', 'Remote Port', 'Remote IP', 'Remote IP Prefix Length', 'DSCP Value', 'Throttle Rate'
+		Type          = 'String'
+		Value         = '1.0', '*', '*', '58000:58019', '*', '*', '*', '*', '*', '34', '-1'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Set-GPRegistryValue @paramSetGPRegistryValue)
+}
+catch
+{
+	Write-Warning 'Unable to create Client Video QoS'
+}
+#endregion ClientVideoQoS
+
+#region ClientApplicationSharingQoS
+try
+{
+	$paramSetGPRegistryValue = @{
+		Name          = $GPOName
+		Key           = 'HKLM\SOFTWARE\Policies\Microsoft\Windows\QoS\Skype for Business Client Application Sharing QoS'
+		ValueName     = 'Version', 'Application Name', 'Protocol', 'Local Port', 'Local IP', 'Local IP Prefix Length', 'Remote Port', 'Remote IP', 'Remote IP Prefix Length', 'DSCP Value', 'Throttle Rate'
+		Type          = 'String'
+		Value         = '1.0', '*', '*', '42000:42019', '*', '*', '*', '*', '*', '24', '-1'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Set-GPRegistryValue @paramSetGPRegistryValue)
+}
+catch
+{
+	Write-Warning 'Unable to create Client Application Sharing QoS'
+}
+#endregion ClientApplicationSharingQoS
+
+#region ClientMediaPortQoS
+try
+{
+	$paramSetGPRegistryValue = @{
+		Name          = $GPOName
+		Key           = 'HKLM\SOFTWARE\Policies\Microsoft\Windows\QoS\Skype for Business Client Media Port QoS'
+		ValueName     = 'Version', 'Application Name', 'Protocol', 'Local Port', 'Local IP', 'Local IP Prefix Length', 'Remote Port', 'Remote IP', 'Remote IP Prefix Length', 'DSCP Value', 'Throttle Rate'
+		Type          = 'String'
+		Value         = '1.0', '*', '*', '5350:5389', '*', '*', '*', '*', '*', '34', '-1'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Set-GPRegistryValue @paramSetGPRegistryValue)
+}
+catch
+{
+	Write-Warning 'Unable to create Client Media Port QoS'
+}
+#endregion ClientMediaPortQoS
---
+++ Skype_for_Business_QoS/Set-SkypeQoS.ps1
@@ -0,0 +1,244 @@
+﻿#Requires -RunAsAdministrator
+
+<#
+		.SYNOPSIS
+		Set QoS Settings for all Skype for Business Server Roles
+
+		.DESCRIPTION
+		Set QoS Settings for all Skype for Business Server Roles, except Edge. This is in a dedicated script
+
+		.EXAMPLE
+		PS C:\> .\Set-SkypeQoS.ps1
+
+		.NOTES
+		Copyright (c) 2017 Joerg Hochwald (http://jhochwald.com). All rights reserved.
+
+		Redistribution and use in source and binary forms, with or without modification,
+		are permitted provided that the following conditions are met:
+
+		1.	Redistributions of source code must retain the above copyright notice,
+		this list of conditions and the following disclaimer.
+
+		2.	Redistributions in binary form must reproduce the above copyright notice, this list of
+		conditions and the following disclaimer in the documentation and/or other materials
+		provided with the distribution.
+
+		3.	Neither the name of the copyright holder nor the names of its contributors may be used
+		to endorse or promote products derived from this software without specific prior
+		written permission.
+
+		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+		IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+		AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+		CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+		CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+		SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+		THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+		OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+		POSSIBILITY OF SUCH DAMAGE.
+
+		By using the Software, you agree to the License, Terms and Conditions above!
+#>
+[CmdletBinding()]
+param ()
+
+#region Variables
+$SC = 'SilentlyContinue'
+$STP = 'Stop'
+#endregion Variables
+
+#region LoadSkypeModule
+try
+{
+	$paramImportModule = @{
+		Name          = 'SkypeforBusiness'
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Import-Module @paramImportModule)
+}
+catch
+{
+	Write-Error -Message 'Skype for Business PowerShell Module not found...' -ErrorAction $STP
+}
+#endregion LoadSkypeModule
+
+#region EnableMediaQoS
+try
+{
+	$paramGetCsMediaConfiguration = @{
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$paramSetCsMediaConfiguration = @{
+		EnableQoS     = $True
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	$null = (Get-CsMediaConfiguration @paramGetCsMediaConfiguration | Set-CsMediaConfiguration @paramSetCsMediaConfiguration)
+
+	Write-Verbose -Message 'Enabled Media Configuration for Quality of Service'
+}
+catch
+{
+	Write-Warning -Message 'To enable Media Configuration for Quality of Service'
+}
+#endregion EnableMediaQoS
+
+#region SetTheMediationServerPortRanges
+try
+{
+	$paramGetCsService = @{
+		MediationServer = $True
+		ErrorAction     = $STP
+		WarningAction   = $SC
+	}
+	Get-CsService @paramGetCsService | ForEach-Object -Process {
+		try
+		{
+			$paramSetCsMediationServer = @{
+				Identity       = $_.Identity
+				AudioPortStart = 49152
+				AudioPortCount = 8348
+				ErrorAction    = $STP
+				WarningAction  = $SC
+			}
+			$null = (Set-CsMediationServer @paramSetCsMediationServer)
+
+			#
+			Write-Verbose -Message ('Set {0} Audio Port Range for Quality of Service' -f $_.Identity)
+		}
+		catch
+		{
+			Write-Warning -Message ('To set {0} for for Quality of Service' -f $_.Identity)
+		}
+	}
+	Write-Verbose -Message 'Configuring Mediation Servers for Quality of Service'
+}
+catch
+{
+	Write-Warning -Message 'To set Mediation servers for Quality of Services'
+}
+#endregion SetTheMediationServerPortRanges
+
+#region SetTheApplicationServersPortRanges
+try
+{
+	$paramGetCsService = @{
+		ApplicationServer = $True
+		ErrorAction       = $STP
+		WarningAction     = $SC
+	}
+	Get-CsService @paramGetCsService | ForEach-Object -Process {
+		try
+		{
+			$paramSetCsApplicationServer = @{
+				Identity            = $_.Identity
+				AudioPortStart      = 49152
+				AudioPortCount      = 8348
+				AppSharingPortStart = 40803
+				AppSharingPortCount = 8348
+				VideoPortStart      = 57501
+				VideoPortCount      = 8034
+				ErrorAction         = $STP
+				WarningAction       = $SC
+			}
+			$null = (Set-CsApplicationServer @paramSetCsApplicationServer)
+
+			#
+			Write-Verbose -Message ('Set {0} Port Ranges for Quality of Service' -f $_.Identity)
+		}
+		catch
+		{
+			Write-Warning -Message ('To set {0} for for Quality of Service' -f $_.Identity)
+		}
+	}
+	Write-Verbose -Message 'Configuring Application Servers for Quality of Service'
+}
+catch
+{
+	Write-Warning -Message 'To set Application servers for Quality of Services'
+}
+#endregion SetTheApplicationServersPortRanges
+
+#region SetConferencingServersPortRanges
+try
+{
+	$paramGetCsService = @{
+		ConferencingServer = $True
+		ErrorAction        = $STP
+		WarningAction      = $SC
+	}
+	Get-CsService @paramGetCsService | ForEach-Object -Process {
+		try
+		{
+			$paramSetCsConferenceServer = @{
+				Identity            = $_.Identity
+				AudioPortStart      = 49152
+				AudioPortCount      = 8348
+				AppSharingPortStart = 40803
+				AppSharingPortCount = 8348
+				VideoPortStart      = 57501
+				VideoPortCount      = 8034
+				ErrorAction         = $STP
+				WarningAction       = $SC
+			}
+			$null = (Set-CsConferenceServer @paramSetCsConferenceServer)
+
+			#
+			Write-Verbose -Message ('Set {0} Port Ranges for Quality of Service' -f $_.Identity)
+		}
+		catch
+		{
+			Write-Warning -Message ('To set {0} for for Quality of Service' -f $_.Identity)
+		}
+	}
+	Write-Verbose -Message 'Configuring Conferencing Servers for Quality of Service'
+}
+catch
+{
+	Write-Warning -Message 'To set Conferencing servers for Quality of Services'
+}
+#endregion SetConferencingServersPortRanges
+
+#region SetClientPortPanges
+try
+{
+	$paramGetCsConferencingConfiguration = @{
+		ErrorAction   = $STP
+		WarningAction = $SC
+	}
+	Get-CsConferencingConfiguration @paramGetCsConferencingConfiguration | ForEach-Object -Process {
+		try
+		{
+			$paramSetCsConferencingConfiguration = @{
+				Identity                    = $_.Identity
+				ClientMediaPortRangeEnabled = $True
+				ClientAudioPort             = 50020
+				ClientAudioPortRange        = 20
+				ClientVideoPort             = 58000
+				ClientVideoPortRange        = 20
+				ClientAppSharingPort        = 42000
+				ClientAppSharingPortRange   = 20
+				ClientFileTransferPort      = 42020
+				ClientFileTransferPortRange = 20
+				ErrorAction                 = $STP
+				WarningAction               = $SC
+			}
+			$null = (Set-CsConferencingConfiguration @paramSetCsConferencingConfiguration)
+
+			#
+			Write-Verbose -Message ('Set {0} Port Ranges for Quality of Service' -f $_.Identity)
+		}
+		catch
+		{
+			Write-Warning -Message ('To set {0} for for Quality of Service' -f $_.Identity)
+		}
+	}
+	Write-Verbose -Message 'Configuring Client Applications for Quality of Service'
+}
+catch
+{
+	Write-Warning -Message 'To set Client Applications for Quality of Services'
+}
+#endregion SetClientPortPanges
