bb17ea568c57ddc4b05950ced88a0028d8285d5a
Joerg Hochwald <joerg@hochwald.net>
2018-12-15 22:03:08 +0100

    Import Approve-WSUSLicenseAgreementAcceptance

---
+++ WSUS/Approve-WSUSLicenseAgreementAcceptance.ps1
@@ -0,0 +1,155 @@
+﻿#requires -Version 3.0 -Modules UpdateServices
+
+<#
+    .SYNOPSIS
+    Accept License Agreements
+	
+    .DESCRIPTION
+    Accept License Agreements for all Windows Server Update Services (WSUS) Updates
+	
+    .PARAMETER Name
+    Specifies the name of a WSUS server.
+
+    .EXAMPLE
+    PS C:\> Approve-WSUSLicenseAgreementAcceptance -Name 'mycdc01'
+
+    Accept License Agreements on the WSUS Server 'mycdc01'
+
+    .EXAMPLE
+    PS C:\> Approve-WSUSLicenseAgreementAcceptance
+
+    Accept License Agreements 
+
+    .NOTES
+    Initial beta Version
+#>
+[CmdletBinding(ConfirmImpact = 'None',
+SupportsShouldProcess)]
+param
+(
+  [Parameter(ValueFromPipeline,
+      ValueFromPipelineByPropertyName,
+  Position = 1)]
+  [Alias('WSUSServer')]
+  [string]
+  $Name = $null
+)
+
+begin
+{
+  try 
+  {
+    if ($Name) 
+    {
+      Write-Verbose -Message ('Use {0} as WSUS Server' -f $Name)
+
+      $paramGetWsusServer = @{
+        Name          = $Name
+        ErrorAction   = 'Stop'
+        WarningAction = 'Continue'
+      }
+    }
+    else 
+    {
+      Write-Verbose -Message 'Use the default WSUS Server'
+      $paramGetWsusServer = @{
+        ErrorAction   = 'Stop'
+        WarningAction = 'Continue'
+      }
+    }
+    $WSUS = (Get-WsusServer @paramGetWsusServer)
+  }
+  catch 
+  {
+    # get error record
+    [Management.Automation.ErrorRecord]$e = $_
+
+    # retrieve information about runtime error
+    $info = [PSCustomObject]@{
+      Exception = $e.Exception.Message
+      Reason    = $e.CategoryInfo.Reason
+      Target    = $e.CategoryInfo.TargetName
+      Script    = $e.InvocationInfo.ScriptName
+      Line      = $e.InvocationInfo.ScriptLineNumber
+      Column    = $e.InvocationInfo.OffsetInLine
+    }
+      
+    # output information. Post-process collected info, and log info (optional)
+    $info
+
+    Write-Error -Message 'No WSUS Server was found!' -ErrorAction Stop
+
+    break
+
+    exit 1
+  }
+
+  $unapprovedUpdates = $null
+  $unapprovedUpdates = $WSUS.getupdates() | Where-Object -FilterScript {
+    $_.isdeclined -ne $true
+  }
+
+  $license = $null
+  if ($unapprovedUpdates) 
+  {
+    $license = $unapprovedUpdates | Where-Object -FilterScript {
+      $_.RequiresLicenseAgreementAcceptance
+    }
+  } else 
+  {
+    Write-Verbose -Message 'Nothing left todo.'
+  }
+}
+
+process
+{
+  if ($license) 
+  {
+    if ($pscmdlet.ShouldProcess("$license", 'Accept License Agreement')) 
+    {
+      $license | ForEach-Object -Process {
+        $_.AcceptLicenseAgreement()
+      }
+    }
+  }
+  else 
+  {
+    Write-Verbose -Message 'Nothing left todo...'
+  }
+}
+
+end
+{
+  Write-Verbose -Message 'Done'
+}
+
+#region License
+<#
+    BSD 3-Clause License
+
+    Copyright (c) 2018, enabling Technology <http://enatec.io>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    By using the Software, you agree to the License, Terms and Conditions above!
+#>
+#endregion License
+
+#region Hints
+<#
+    This is a third-party Software!
+
+    The developer(s) of this Software is NOT sponsored by or affiliated with Microsoft Corp (MSFT) or any of its subsidiaries in any way
+
+    The Software is not supported by Microsoft Corp (MSFT)!
+#>
+#endregion Hints
