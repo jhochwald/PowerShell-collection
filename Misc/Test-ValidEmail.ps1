1debf73f8ee47298e963e7a2470e90a257795db1
Joerg Hochwald <joerg@hochwald.net>
2018-12-17 02:21:35 +0100

    Initial Import of Test-ValidEmail.ps1

---
+++ Misc/Test-ValidEmail.ps1
@@ -0,0 +1,81 @@
+﻿#requires -Version 3.0
+function Test-ValidEmail
+{
+  <#
+      .SYNOPSIS
+      Simple Function to check if a String is a valid Mail
+	
+      .DESCRIPTION
+      Simple Function to check if a String is a valid Mail and return a Bool
+	
+      .PARAMETER address
+      Address String to Check
+
+      .OUTPUT
+      Bool
+
+      .INPUT
+      String
+	
+      .EXAMPLE
+      # Not a valid String
+      PS C:\> Test-ValidEmail -address 'Joerg.Hochwald'
+      False
+	
+      .EXAMPLE
+      # Valid String
+      PS C:\> Test-ValidEmail -address 'Joerg.Hochwald@outlook.com'
+      True
+	
+      .NOTES
+      Disclaimer: The code is provided 'as is,' with all possible faults, defects or errors, and without warranty of any kind.
+		
+      Author: Joerg Hochwald
+  #>
+	
+  [OutputType([bool])]
+  param
+  (
+    [Parameter(Mandatory,
+    HelpMessage = 'Address String to Check')]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $address
+  )
+
+  process
+  {
+    ($address -as [mailaddress]).Address -eq $address -and $address -ne $null
+  }
+}
+
+#region License
+<#
+    BSD 3-Clause License
+
+    Copyright (c) 2018, enabling Technology <http://enatec.io>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    By using the Software, you agree to the License, Terms and Conditions above!
+#>
+#endregion License
+
+#region Hints
+<#
+    This is a third-party Software!
+
+    The developer(s) of this Software is NOT sponsored by or affiliated with Microsoft Corp (MSFT) or any of its subsidiaries in any way
+
+    The Software is not supported by Microsoft Corp (MSFT)!
+#>
+#endregion Hints
