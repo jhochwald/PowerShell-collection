8441504c32806ead97eed8c09e75bda8664165c5
Joerg Hochwald <joerg@hochwald.net>
2018-12-22 15:10:42 +0100

    minor code reformating and very minor refactoring

--- Misc/Grant-LogOnAsService.ps1
+++ Misc/Grant-LogOnAsService.ps1
@@ -1,4 +1,4 @@
-﻿#requires -Version 3.0
+﻿#requires -Version 3.0 -RunAsAdministrator
 
 function Grant-LogOnAsService
 {
@@ -43,7 +43,7 @@ function Grant-LogOnAsService
     [string[]]
     $Users
   )
-
+	
   process
   {
     if ($pscmdlet.ShouldProcess('Apply login as a service', "$Users"))
@@ -53,25 +53,27 @@ function Grant-LogOnAsService
       $curSIDs = (Select-String -Path .\tempexport.inf -Pattern 'SeServiceLogonRight')
       $Sids = $curSIDs.line
       $sidstring = ''
-			
+
       foreach ($user in $Users)
       {
         $objUser = (New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList ($user))
         $strSID = $objUser.Translate([Security.Principal.SecurityIdentifier])
-				
+
         if (!$Sids.Contains($strSID) -and !$Sids.Contains($user))
         {
           $sidstring += ",*$strSID"
         }
       }
-			
+
       if ($sidstring)
       {
         $newSids = $Sids + $sidstring
+
         Write-Output -InputObject ('New Sids: {0}' -f $newSids)
         $tempinf = (Get-Content -Path tempexport.inf)
         $tempinf = $tempinf.Replace($Sids, $newSids)
-        Add-Content -Path tempimport.inf -Value $tempinf
+        $null = (Add-Content -Path tempimport.inf -Value $tempinf -Force -Confirm:$false)
+
         & "$env:windir\system32\secedit.exe" /import /db secedit.sdb /cfg '.\tempimport.inf'
         & "$env:windir\system32\secedit.exe" /configure /db secedit.sdb
         & "$env:windir\system32\gpupdate.exe" /force
@@ -82,14 +84,21 @@ function Grant-LogOnAsService
       }
     }
   }
-	
+
   end
   {
     if ($pscmdlet.ShouldProcess('Cleanup', 'Tempfiles'))
     {
-      $null = (Remove-Item -Path '.\tempimport.inf' -Force -ErrorAction SilentlyContinue)
-      $null = (Remove-Item -Path '.\secedit.sdb' -Force -ErrorAction SilentlyContinue)
-      $null = (Remove-Item -Path '.\tempexport.inf' -Force -ErrorAction SilentlyContinue)
+      # Splat the Defaults
+      $paramRemoveItem = @{
+        Force       = $true
+        Confirm     = $false
+        ErrorAction = 'SilentlyContinue'
+      }
+
+      $null = (Remove-Item -Path '.\tempimport.inf' @paramRemoveItem)
+      $null = (Remove-Item -Path '.\secedit.sdb' @paramRemoveItem)
+      $null = (Remove-Item -Path '.\tempexport.inf' @paramRemoveItem)
     }
   }
 }
