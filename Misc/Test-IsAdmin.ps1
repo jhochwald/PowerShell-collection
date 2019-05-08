f6c1ef37ab40b634e82ed815236b06d3de0dfbd6
joerghochwald <joerg@hochwald.net>
2019-05-08 16:38:23 +0200

    Check if PowerShell run elevated (e.g. as admin or not) It runs on PowerShell and PowerShell Core, and it supports macOS or Linux as well.

---
+++ Misc/Test-IsAdmin.ps1
@@ -0,0 +1,68 @@
+﻿function Test-IsAdmin
+{
+<#
+	.SYNOPSIS
+		Check if PowerShell run elevated (e.g. as admin or not)
+	
+	.DESCRIPTION
+		This is a complete new approach to check if the Shell runs elevated or not.
+		It runs on PowerShell and PowerShell Core, and it supports macOS or Linux as well.
+	
+	.EXAMPLE
+		PS C:\> Test-IsAdmin
+	
+	.NOTES
+		Rewritten function to support PowerShell Desktop and Core on Windows, macOS, and Linux
+		Mostly used within other functions and in the personal PowerShell profiles.
+
+		Version: 1.0.0
+		
+      GUID: a59bfa91-7206-4892-bc2a-acf666b35364
+		
+      Author: Joerg Hochwald
+		
+      Companyname: Alright IT GmbH
+		
+      Copyright: Copyright (c) 2019, Alright IT GmbH - All rights reserved.
+		
+      License: https://opensource.org/licenses/BSD-3-Clause
+		
+      Releasenotes:
+      1.0.0 2019-05-09: Initial Release of the rewritten function
+		
+      THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+#>
+	
+	[CmdletBinding(ConfirmImpact = 'None')]
+	[OutputType([bool])]
+	param ()
+	
+	process
+	{
+		if ($PSVersionTable.PSEdition -eq 'Desktop')
+		{
+			([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+		}
+		elseif (($PSVersionTable.PSEdition -eq 'Core') -and ($PSVersionTable.Platform -eq 'Unix'))
+		{
+			if ((id -u) -eq 0)
+			{
+				return $true
+			}
+			else
+			{
+				return $false
+			}
+		}
+		elseif (($PSVersionTable.PSEdition -eq 'Core') -and ($PSVersionTable.Platform -eq 'Win32NT'))
+		{
+			([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+		}
+		else
+		{
+			Write-Warning -Message 'Unknown'
+			
+			return
+		}
+	}
+}
