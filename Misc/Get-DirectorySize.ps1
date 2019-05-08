022b339279c763ee524006a4886d4c5e61efc2fa
joerghochwald <joerg@hochwald.net>
2019-05-08 17:41:06 +0200

    Remove Whitespaces (no code changes)

--- Misc/Get-DirectorySize.ps1
+++ Misc/Get-DirectorySize.ps1
@@ -12,36 +12,36 @@
 
          .PARAMETER Type
          Type of the Return,
-         Vaild values are: GB, MB, KB, B
+         Valid values are: GB, MB, KB, B
          The default is MB (Megabyte)
-	
+
          .EXAMPLE
          PS C:\> Get-DirectorySize -Path 'C:\scripts'
 
          .EXAMPLE
          PS C:\> Get-DirectorySize -Path 'C:\scripts' -Type GB
-	
+
          .NOTES
          PowerShell function to emulate the wel known Linux DU command
 
          Version: 1.0.0
-		
+
          GUID: e559f754-f1aa-4376-8c28-dea035fe2c5a
-	
+
          Author: Joerg Hochwald
-	
+
          Companyname: Alright IT GmbH
-	
+
          Copyright: Copyright (c) 2019, Alright IT GmbH - All rights reserved.
-	
+
          License: https://opensource.org/licenses/BSD-3-Clause
-	
+
          Releasenotes:
          1.0.0 2019-05-09: Initial Release
-	
+
          THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
    #>
-	
+
    [CmdletBinding(ConfirmImpact = 'None')]
    [OutputType([string])]
    param
@@ -60,13 +60,13 @@
       [string]
       $Type = 'MB'
    )
-	
+
    process
    {
       try
       {
          $AllFolderItems = (Get-ChildItem -Path $Path -Recurse -ErrorAction Stop | Measure-Object -Property length -Sum)
-			
+
          switch ($Type)
          {
             'GB'
@@ -97,7 +97,7 @@
       {
          # get error record
          [Management.Automation.ErrorRecord]$e = $_
-			
+
          # retrieve information about runtime error
          $info = [PSCustomObject]@{
             Exception = $e.Exception.Message
@@ -107,10 +107,10 @@
             Line      = $e.InvocationInfo.ScriptLineNumber
             Column    = $e.InvocationInfo.OffsetInLine
          }
-			
+
          # output information. Post-process collected info, and log info (optional)
          $info | Out-String | Write-Verbose
-			
+
          Write-Error -Message $e.Exception.Message -ErrorAction Continue -Exception $e.Exception -TargetObject $e.CategoryInfo.TargetName
       }
    }
