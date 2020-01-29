6d79d36bfce61558525992b14eb008a929275532
joerghochwald <joerg@hochwald.net>
2020-01-29 18:04:11 +0100

    Add Get-IpInfo
    Get all local IP addresses, just the addresses

---
+++ Misc/Get-IpInfo.ps1
@@ -0,0 +1,42 @@
+﻿<#
+      .SYNOPSIS
+      Get all local IP addresses
+
+      .DESCRIPTION
+      Get all local IP addresses, just the addresses
+
+      .EXAMPLE
+      PS C:\> .\Get-IpInfo.ps1
+
+      Get all local IP addresses, just the addresses
+
+      .NOTES
+      Quick an dirty function that uses Net.DNS to gather the information about the IP Addresses
+#>
+[CmdletBinding(ConfirmImpact = 'None')]
+[OutputType([psobject])]
+param ()
+
+begin {
+   #Cleanup
+   $IpAddressInfo = $null
+}
+
+process {
+   # Get the Info using Net.Dns
+   $IpAddressInfo = @(
+      (([Net.DNS]::GetHostAddresses([Net.Dns]::GetHostByName(($env:COMPUTERNAME)).HostName) | Where-Object -FilterScript {
+               $_.IsIPv6LinkLocal -eq $false
+         }).IPAddressToString | Where-Object -FilterScript {
+            $_ -ne '::1'
+      })
+   )
+}
+
+end {
+   # Dump the Info
+   $IpAddressInfo
+
+   #Cleanup
+   $IpAddressInfo = $null
+}
