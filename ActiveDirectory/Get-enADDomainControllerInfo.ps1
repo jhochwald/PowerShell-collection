94a658bcd89f8fb0d5073ec7a6f50d9881e99c21
joerghochwald <joerg@hochwald.net>
2020-01-29 16:37:20 +0100

    Add Get-enDomainControllerInfo
    Get a list of domain controllers

---
+++ ActiveDirectory/Get-enADDomainControllerInfo.ps1
@@ -0,0 +1,69 @@
+﻿Function Get-enDomainControllerInfo
+{
+   <#
+         .SYNOPSIS
+         Get a list of domain controllers
+
+         .DESCRIPTION
+         Will provide a list of domain controllers in your current domain.
+         Optionally you can also request a discovery of the "closest" one.
+
+         .PARAMETER ComputerName
+         Retrieve information about the specified domain controller.
+         This is a RegEx match so you can match multiple domain controllers with your pattern.
+
+         .PARAMETER Discover
+         Use Discover to return the information of the closest domain controller.
+
+         .EXAMPLE
+         Get-enDomainControllerInfo
+
+         Retrieve a list of all domain controllers in your domain.
+
+         .EXAMPLE
+         Get-enDomainControllerInfo -Computer 01
+
+         Retrieve a list of all domain controlelrs with "01" in their name.
+
+         .EXAMPLE
+         Get-enDomainControllerInfo -Discover
+    
+         Retrieve the name of the closest domain controller.
+
+         .NOTES
+   #>
+   [CmdletBinding(DefaultParameterSetName = 'all')]
+   Param (
+      [Parameter(Position = 0,ParameterSetName = 'dc')]
+      [string]$ComputerName,
+      [Parameter(ParameterSetName = 'all')]
+      [switch]$Discover
+   )
+
+   begin {
+      $DirectoryContext = [DirectoryServices.ActiveDirectory.DirectoryContext]::New('Domain')
+      $SelectProperties = 'Name', 'Forest', 'Domain', 'SiteName', 'Roles', 'CurrentTime', 'HighestCommittedUsn', 'OSVersion'
+   }
+
+   process {
+      If ($Discover) 
+      {
+         $LocatorFlag = [DirectoryServices.ActiveDirectory.LocatorOptions]::ForceRediscovery
+         $Info = ([DirectoryServices.ActiveDirectory.DomainController]::FindOne($DirectoryContext, $LocatorFlag) | Select-Object -Property $SelectProperties)
+      } elseif ($ComputerName) 
+      {
+         $Info = ([DirectoryServices.ActiveDirectory.DomainController]::FindAll($DirectoryContext) | Where-Object -Property Name -Match -Value $ComputerName | Select-Object -Property $SelectProperties)
+      } else 
+      {
+         $Info = ([DirectoryServices.ActiveDirectory.DomainController]::FindAll($DirectoryContext) | Select-Object -Property $SelectProperties)
+      }
+   }
+
+   end {
+      $Info
+   }
+}
+
+
+
+Get-enDomainControllerInfo
