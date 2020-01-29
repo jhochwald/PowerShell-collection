a03d6935fe2b686557a4e70ebd72d7f1a356889e
joerghochwald <joerg@hochwald.net>
2020-01-29 18:15:24 +0100

    Tweak UDP Performance
    Based on some VMWare recommendations

---
+++ Misc/Tweak_UDPforFastSend.ps1
@@ -0,0 +1,59 @@
+﻿# Increases the UDP packet size to 1500 bytes for FastSend
+# http://kb.vmware.com/selfservice/microsites/search.do?language=en_US&cmd=displayKC&externalId=2040065
+$blnIncreaseFastSendDatagramThreshold = $true
+
+if ($blnIncreaseFastSendDatagramThreshold)
+{
+   #Inform user
+   Write-Output -InputObject 'Increasing UDP FastSend threshold'
+
+   $RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Services\AFD\Parameters'
+   $RegistryName = 'FastSendDatagramThreshold'
+   $RegistryValue = '1500'
+
+   If (Test-Path -Path $RegistryPath)
+   {
+      $null = (New-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $RegistryValue -PropertyType DWORD -Force -Confirm:$false)
+      Write-Output -InputObject '(CREATED)'
+   }
+   else
+   {
+      $null = (Set-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $RegistryValue -Force -Confirm:$false)
+      Write-Output -InputObject '(MODIFIED)'
+   }
+}
+else
+{
+   Write-Warning -Message '(skipped)'
+}
+
+# Set multiplication factor to the default UDP scavenge value (MaxEndpointCountMult)
+# http://support.microsoft.com/kb/2685007/en-us
+$lbnSetMaxEndpointCountMult = $true
+
+if ($lbnSetMaxEndpointCountMult)
+{
+   #Inform user
+   Write-Output -InputObject 'Set multiplication factor to the default UDP scavenge value'
+
+   $RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Services\BFE\Parameters'
+   $RegistryName = 'MaxEndpointCountMult'
+   $RegistryValue = '0x10'
+
+   If (Test-Path -Path $RegistryPath)
+   {
+      $null = (New-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $RegistryValue -PropertyType DWORD -Force -Confirm:$false)
+
+      Write-Output -InputObject '(CREATED)'
+   }
+   else
+   {
+      $null = (Set-ItemProperty -Path $RegistryPath -Name $RegistryName -Value $RegistryValue -Force -Confirm:$false)
+
+      Write-Output -InputObject '(MODIFIED)'
+   }
+}
+else
+{
+   Write-Warning -Message '(skipped)'
+}
