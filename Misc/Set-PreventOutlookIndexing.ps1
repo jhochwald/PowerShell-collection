0001e520dbbf572c7095518af8d9ddea86ce63fc
Joerg Hochwald <jhochwald@users.noreply.github.com>
2022-03-11 22:11:34 +0100

    Prevent indexing Microsoft Office Outlook
    KB5008212 Windows security update breaks Outlook search

---
+++ Misc/Set-PreventOutlookIndexing.ps1
@@ -0,0 +1,38 @@
+﻿#requires -Version 1.0
+
+function Set-PreventOutlookIndexing
+{
+   <#
+         .SYNOPSIS
+         Prevent indexing Microsoft Office Outlook
+
+         .DESCRIPTION
+         Prevent indexing Microsoft Office Outlook
+
+         .EXAMPLE
+         PS C:\> .\Set-PreventOutlookIndexing
+
+         Prevent indexing Microsoft Office Outlook
+
+         .NOTES
+         KB5008212 Windows security update breaks Outlook search
+
+         .LINK
+         https://www.catalog.update.microsoft.com/Search.aspx?q=KB5008212
+   #>
+
+   try
+   {
+      $null = (New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\Windows Search' -Name PreventIndexingOutlook -PropertyType DWord -Value 1 -ErrorAction Stop)
+
+      Write-Verbose -Message 'Success... Prevent-OutlookIndexing'
+   }
+   catch
+   {
+      $errorMessage = $_.Exception.Message
+      Write-Warning -Message ('Exception thrown in Set-PreventOutlookIndexing; {0}' -f $errorMessage)
+      $errorMessage = $null
+   }
+}
+
+Set-PreventOutlookIndexing
