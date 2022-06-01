69ff0003d2900255eea3168c95466e9e4a43b04d
Joerg Hochwald <joerg.hochwald@enabling.technology>
2022-06-01 10:43:37 +0200

    CVE-2022-30190 detection and remediation

---
+++ Intune/ProactiveRemediations/Detection-MitigateCVE202230190.ps1
@@ -0,0 +1,42 @@
+﻿#region Check
+#region Defaults
+$SCT = 'SilentlyContinue'
+#endregion Defaults
+
+# Clean-up
+$RegistryRoot = $null
+
+# Figure out if we have an existing PowerShell Registry Provider mapping
+$paramGetPSDrive = @{
+    ErrorAction   = $SCT
+    WarningAction = $SCT
+}
+$RegistryRoot = ((Get-PSDrive @paramGetPSDrive | Where-Object {
+        $PSItem.Root -eq 'HKEY_CLASSES_ROOT'
+        }).Name)
+$paramGetPSDrive = $null
+
+if (-not ($RegistryRoot))
+{
+    # PowerShell Registry Provider
+    $paramNewPSDrive = @{
+        PSProvider  = 'registry'
+        Root        = 'HKEY_CLASSES_ROOT'
+        Name        = 'HKCR'
+        ErrorAction = 'Stop'
+    }
+    $RegistryRoot = ((New-PSDrive @paramNewPSDrive).Name)
+    $paramNewPSDrive = $null
+}
+
+If (Get-Item -Path ('{0}:\ms-msdt' -f $RegistryRoot) -ErrorAction $SCT)
+{
+    return $false
+}
+else
+{
+    return $true
+}
+
+return $true
+#endregion Check
---
+++ Intune/ProactiveRemediations/Remediation-MitigateCVE202230190.ps1
@@ -0,0 +1,64 @@
+﻿#region Remediation
+#region Defaults
+$STP = 'Stop'
+$SCT = 'SilentlyContinue'
+#endregion Defaults
+
+# Clean-up
+$RegistryRoot = $null
+
+# Figure out if we have an existing PowerShell Registry Provider mapping
+$paramGetPSDrive = @{
+    ErrorAction   = $SCT
+    WarningAction = $SCT
+}
+$RegistryRoot = ((Get-PSDrive @paramGetPSDrive | Where-Object {
+        $PSItem.Root -eq 'HKEY_CLASSES_ROOT'
+        }).Name)
+$paramGetPSDrive = $null
+
+if (-not ($RegistryRoot))
+{
+    # PowerShell Registry Provider
+    $paramNewPSDrive = @{
+        PSProvider  = 'registry'
+        Root        = 'HKEY_CLASSES_ROOT'
+        Name        = 'HKCR'
+        ErrorAction = $STP
+    }
+    $RegistryRoot = ((New-PSDrive @paramNewPSDrive).Name)
+    $paramNewPSDrive = $null
+}
+
+try
+{
+    # Guidance for CVE-2022-30190 Microsoft Support Diagnostic Tool Vulnerability
+    $paramRemoveItem = @{
+        Path        = ('{0}:\ms-msdt' -f $RegistryRoot)
+        Force       = $true
+        Recurse     = $true
+        ErrorAction = $STP
+    }
+    $null = (Remove-Item @paramRemoveItem)
+    $paramRemoveItem = $null
+}
+catch
+{
+    [Management.Automation.ErrorRecord]$e = $_
+
+    $info = [PSCustomObject]@{
+        Exception = $e.Exception.Message
+        Reason    = $e.CategoryInfo.Reason
+        Target    = $e.CategoryInfo.TargetName
+        Script    = $e.InvocationInfo.ScriptName
+        Line      = $e.InvocationInfo.ScriptLineNumber
+        Column    = $e.InvocationInfo.OffsetInLine
+    }
+
+    Write-Warning -Message $info -WarningAction $STP
+
+    exit 1
+}
+
+return $true
+#endregion Remediation
