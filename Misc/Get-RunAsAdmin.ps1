bfd5edaaa637434074ef112d3a4847f03e757ff4
Joerg Hochwald <jhochwald@users.noreply.github.com>
2022-07-28 00:09:59 +0200

    Add some minor tools
    Add two new functions

---
+++ Misc/Get-RunAsAdmin.ps1
@@ -0,0 +1,24 @@
+﻿function Get-RunAsAdmin
+{
+   <#
+      .SYNOPSIS
+      Check if the the process runs as Admin (Elevated)
+
+      .DESCRIPTION
+      Check if the the process runs as Admin (Elevated)
+
+      .EXAMPLE
+      Get-RunAsAdmin
+      Check if the the process runs as Admin (Elevated)
+
+      .NOTES
+      Easy check
+   #>
+   [CmdletBinding(ConfirmImpact = 'None')]
+   [OutputType([bool])]
+   param ()
+
+   process {
+      [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'
+   }
+}
---
+++ Misc/Get-RunAsSystem.ps1
@@ -0,0 +1,31 @@
+﻿function Get-RunAsSystem
+{
+   <#
+      .SYNOPSIS
+      Check if the the process runs as System
+
+      .DESCRIPTION
+      Check if the the process runs as System
+
+      .EXAMPLE
+      Get-RunAsSystem
+      Check if the the process runs as System
+
+      .NOTES
+      Easy check
+   #>
+   [CmdletBinding(ConfirmImpact = 'None')]
+   [OutputType([bool])]
+   param ()
+
+   process {
+      if ((& "$env:windir\system32\whoami.exe") -eq 'nt authority\system')
+      {
+         $true
+      }
+      else
+      {
+         $false
+      }
+   }
+}
