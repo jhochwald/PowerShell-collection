61f3fc69312e3591378114cc3fe524d0f2841fcd
Joerg Hochwald <jhochwald@users.noreply.github.com>
2022-10-04 14:13:11 +0200

    Force an update of all Exchange Address books

---
+++ Exchange/Invoke-UpdateAllExchangeAdressBooks.ps1
@@ -0,0 +1,75 @@
+﻿#requires -Version 1.0
+
+<#
+.SYNOPSIS
+Force an update of all Exchange Addressbooks
+
+.DESCRIPTION
+Force an update of all Exchange Addressbooks
+Works with an On Premises only and Hybrid installtion.
+
+.EXAMPLE
+PS C:\> .\Invoke-UpdateAllExchangeAdressBooks.ps1
+
+.NOTES
+You must execute this within an Exchange Management Shell, not Exchange Online Shell!
+Works with an On Premises only and Hybrid installtion.
+#>
+[CmdletBinding(ConfirmImpact = 'None')]
+[OutputType([string])]
+param ()
+
+begin
+{
+   if (-not (Get-Command -Name Get-ExchangeServer -ErrorAction SilentlyContinue))
+   {
+      Write-Error -Exception 'Required cmdlet not found' -Message 'Ensure to run this in the Exchange on premises Management Shell' -Category ResourceUnavailable -RecommendedAction 'Retry within the Exchange Management Shell' -ErrorAction Stop
+      exit 1
+   }
+}
+
+process
+{
+   # Update the recipients included in address lists
+   Get-AddressList | Update-AddressList -Confirm:$false
+   
+   # Update the recipients included in a global address list (GAL)
+   Get-GlobalAddressList | Update-GlobalAddressList -Confirm:$false
+   
+   # Update the offline address books (OABs) used by Microsoft Outlook clients.
+   Get-OfflineAddressBook | Update-OfflineAddressBook -Force -Confirm:$false
+}
+
+end
+{
+}
+
+#region LICENSE
+<#
+   BSD 3-Clause License
+
+   Copyright (c) 2022, enabling Technology GmbH
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+   3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#>
+#endregion LICENSE
+
+#region DISCLAIMER
+<#
+   DISCLAIMER:
+   - Use at your own risk, etc.
+   - This is open-source software, if you find an issue try to fix it yourself. There is no support and/or warranty in any kind
+   - This is a third-party Software
+   - The developer of this Software is NOT sponsored by or affiliated with Microsoft Corp (MSFT) or any of its subsidiaries in any way
+   - The Software is not supported by Microsoft Corp (MSFT)
+   - By using the Software, you agree to the License, Terms, and any Conditions declared and described above
+   - If you disagree with any of the terms, and any conditions declared: Just delete it and build your own solution
+#>
+#endregion DISCLAIMER
