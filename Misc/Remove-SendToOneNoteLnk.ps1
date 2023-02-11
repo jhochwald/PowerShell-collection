187d675ea87bcccc0654775337f66ab8a9267947
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-02-11 16:00:11 +0100

    Create Remove-SendToOneNoteLnk.ps1
    Removes the Send to OneNote Autostart, if it exists

---
+++ Misc/Remove-SendToOneNoteLnk.ps1
@@ -0,0 +1,38 @@
+﻿<#
+      .SYNOPSIS
+      Removes the Send to OneNote Autostart
+
+      .DESCRIPTION
+      Removes the Send to OneNote Autostart, if it exists
+
+      .EXAMPLE
+      PS C:\> .\Remove-SendToOneNoteLnk.ps1
+
+      Removes the Send to OneNote Autostart, if it exists
+
+      .NOTES
+      Simple Helper
+#>
+[CmdletBinding(ConfirmImpact = 'None',
+               SupportsShouldProcess)]
+[OutputType([string])]
+param ()
+
+process
+{
+   $SendToOneNoteLnk = ("{0}\Microsoft\Windows\Start Menu\Programs\Startup\Send to OneNote.lnk" -f $env:APPDATA)
+   
+   if (Test-Path -Path $SendToOneNoteLnk -ErrorAction SilentlyContinue)
+   {
+      if ($pscmdlet.ShouldProcess($SendToOneNoteLnk, 'Remove'))
+      {
+         $paramRemoveItem = @{
+            Path        = $SendToOneNoteLnk
+            Force       = $true
+            Confirm     = $false
+            ErrorAction = 'SilentlyContinue'
+         }
+         $null = (Remove-Item @paramRemoveItem)
+      }
+   }
+}
