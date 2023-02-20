0a012956ba5fd1c84619f6cca1afa8afd2b4b170
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-02-20 13:29:23 +0100

    Get the Azure Active Directory Teant ID for a given Domain
     Do not abuse!

---
+++ AzureAD/Get-AzureADTeantIdForDomain.ps1
@@ -0,0 +1,53 @@
+﻿function Get-AzureADTeantIdForDomain
+{
+   <#
+         .SYNOPSIS
+         Get the Azure Active Directory Teant ID for a given Domain
+   
+         .DESCRIPTION
+         Get the Azure Active Directory Teant ID for a given Domain
+   
+         .PARAMETER Domain
+         TLD Name (Vanity and onmicrosoft.com domains will work)
+         e.g., contoso.com or contoso.onmicrosoft.com
+   
+         .EXAMPLE
+         PS C:\> Get-AzureADTeantIdForDomain -Domain 'contoso.com'
+
+         Get the Azure Active Directory Teant ID for a given Domain
+
+         .EXAMPLE
+         PS C:\> 'contoso.onmicrosoft.com' | Get-AzureADTeantIdForDomain
+
+         Get the Azure Active Directory Teant ID for a given Domain, via pipeline
+   
+         .NOTES
+         Do not abuse!
+
+         Vanity and onmicrosoft.com domains will work
+   #>
+   [CmdletBinding(ConfirmImpact = 'None')]
+   [OutputType([string])]
+   param
+   (
+      [Parameter(ValueFromPipeline,
+                 ValueFromPipelineByPropertyName)]
+      [ValidateNotNullOrEmpty()]
+      [string]
+      $Domain = 'contoso.com'
+   )
+   
+   process
+   {
+      $TeantId = $null
+      $TeantId = ((Invoke-RestMethod -Uri ('https://login.windows.net/{0}/.well-known/openid-configuration' -f $Domain) -Method Get).token_endpoint.Split('/')[3])
+   }
+   
+   end
+   {
+      $TeantId
+      
+      # Cleanup-Up
+      $TeantId = $null
+   }
+}
