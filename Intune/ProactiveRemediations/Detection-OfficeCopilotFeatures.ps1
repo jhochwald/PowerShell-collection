66018d9a177ec985ad78cb78dadf4a2a60180947
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-04-14 19:19:27 +0200

    Update via upstream repo

--- Intune/ProactiveRemediations/Detection-OfficeCopilotFeatures.ps1
+++ Intune/ProactiveRemediations/Detection-OfficeCopilotFeatures.ps1
@@ -2,6 +2,9 @@
    Detection: Enable or disable Copilot Preview Feature in Office
 #>
 
+# Enable or disable Copilot Preview with this bool
+$EnableCopilot = 'true'
+
 try
 {
    #region ExternalFeatureOverrides
@@ -10,44 +13,44 @@ try
       exit 1
    }
    #endregion ExternalFeatureOverrides
-   
+
    #region ExternalFeatureOverridesExcel
    $RegistryPath = 'HKCU:\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\ExternalFeatureOverrides\excel'
-   
+
    if (-not (Test-Path -LiteralPath $RegistryPath -ErrorAction Stop))
    {
       exit 1
    }
-   
-   if (-not ((Get-ItemPropertyValue -LiteralPath $RegistryPath -Name 'Microsoft.Office.Excel.CopilotExperiment' -ErrorAction Stop) -eq 'true'))
+
+   if (-not ((Get-ItemPropertyValue -LiteralPath $RegistryPath -Name 'Microsoft.Office.Excel.CopilotExperiment' -ErrorAction Stop) -eq $EnableCopilot))
    {
       exit 1
    }
    #endregion ExternalFeatureOverridesExcel
-   
+
    #region ExternalFeatureOverridesOneNote
    $RegistryPath = 'HKCU:\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\ExternalFeatureOverrides\onenote'
-   
+
    if (-not (Test-Path -LiteralPath $RegistryPath -ErrorAction Stop))
    {
       exit 1
    }
-   
-   if (-not ((Get-ItemPropertyValue -LiteralPath $RegistryPath -Name 'Microsoft.Office.OneNote.Copilot' -ErrorAction Stop) -eq 'true'))
+
+   if (-not ((Get-ItemPropertyValue -LiteralPath $RegistryPath -Name 'Microsoft.Office.OneNote.Copilot' -ErrorAction Stop) -eq $EnableCopilot))
    {
       exit 1
    }
    #endregion ExternalFeatureOverridesOneNote
-   
+
    #region ExternalFeatureOverridesPowerPoint
    $RegistryPath = 'HKCU:\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\ExternalFeatureOverrides\powerpoint'
-   
+
    if (-not (Test-Path -LiteralPath $RegistryPath -ErrorAction Stop))
    {
       exit 1
    }
-   
-   if (-not ((Get-ItemPropertyValue -LiteralPath $RegistryPath -Name 'Microsoft.Office.PowerPoint.CopilotExperiment' -ErrorAction Stop) -eq 'true'))
+
+   if (-not ((Get-ItemPropertyValue -LiteralPath $RegistryPath -Name 'Microsoft.Office.PowerPoint.CopilotExperiment' -ErrorAction Stop) -eq $EnableCopilot))
    {
       exit 1
    }
