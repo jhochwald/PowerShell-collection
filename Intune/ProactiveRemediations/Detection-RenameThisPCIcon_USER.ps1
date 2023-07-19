02615491c062572775ffaf10653a1176d473475d
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-07-19 12:57:49 +0200

    Rename THIS PC to match the Hostname
    Can be handy, mostly when using Remote Desktop

---
+++ Intune/ProactiveRemediations/Detection-RenameThisPCIcon_USER.ps1
@@ -0,0 +1,21 @@
+﻿$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}'
+$ThisPCName = $env:COMPUTERNAME
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction Stop))
+   {
+      exit 1
+   }
+
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name '(default)' -ErrorAction Stop) -eq $ThisPCName))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
