ce6bfe42ea677fe704d76497c96320c634cba1e2
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-07-19 12:57:20 +0200

    Show THIS PC on Users Desktop
    We want to enable this, because we rename it later to match the Hostname

---
+++ Intune/ProactiveRemediations/Detection-ShowThisPCIcon_USER.ps1
@@ -0,0 +1,20 @@
+﻿$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction Stop))
+   {
+      exit 1
+   }
+
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -ErrorAction Stop) -eq 0))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-ShowThisPCIcon_USER.ps1
@@ -0,0 +1,23 @@
+﻿$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
+
+if ((Test-Path -LiteralPath $RegPath) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = '{20D04FE0-3AEA-1069-A2D8-08002B30309D}'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
