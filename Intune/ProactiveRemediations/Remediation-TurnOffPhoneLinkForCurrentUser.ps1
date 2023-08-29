a55bb5a87e5205c9ea69c5f2bc7cf5ca7c8eef02
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:26 +0200

    Create Remediation-TurnOffPhoneLinkForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Remediation-TurnOffPhoneLinkForCurrentUser.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Turn off Phone Link for current user
+# Remediation-TurnOffPhoneLinkForCurrentUser
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Mobility'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'PhoneLinkEnabled'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
