d265b988c9b841452609c91995744882b3a54e5d
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 20:18:43 +0200

    Create Detection-RemoveWindowsWatermarkOnDesktopForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Detection-RemoveWindowsWatermarkOnDesktopForCurrentUser.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 5.0
+
+# Remove Windows Edition and Build Watermark on Desktop for Current User
+# Detection-RemoveWindowsWatermarkOnDesktopForCurrentUser
+
+$RegPath = 'HKCU:\Control Panel\Desktop'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'PaintDesktopVersion' -ErrorAction SilentlyContinue) -eq 0))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
