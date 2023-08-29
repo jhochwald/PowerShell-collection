5ae9c6a1b1b0f7f350041d5e9f64a2492930c422
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 19:39:10 +0200

    Create Remediation-TurnOnLocalSecurityAuthorityProtectionWithoutUefiLock.ps1

---
+++ Intune/ProactiveRemediations/Remediation-TurnOnLocalSecurityAuthorityProtectionWithoutUefiLock.ps1
@@ -0,0 +1,39 @@
+﻿#requires -Version 1.0
+
+# Turn On Local Security Authority (LSA) Protection without UEFI Lock
+# Remediation-TurnOnLocalSecurityAuthorityProtectionWithoutUefiLock
+
+$RegPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'RunAsPPL'
+   Value        = 2
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'RunAsPPLBoot'
+   Value        = 2
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
