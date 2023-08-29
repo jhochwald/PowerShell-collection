768600bd381ec71e40382d53e1fabec094189017
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:38 +0200

    Create Remediation-UACDefault.ps1

---
+++ Intune/ProactiveRemediations/Remediation-UACDefault.ps1
@@ -0,0 +1,50 @@
+﻿#requires -Version 1.0
+
+# UAC "Notify me only when apps try to make changes to my computer (default)"
+# Remediation-UACDefault
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'PromptOnSecureDesktop'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'EnableLUA'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'ConsentPromptBehaviorAdmin'
+   Value        = 5
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
