43007add58210e00d37670db357596b723ee8d0f
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:41:54 +0200

    Create Remediation-EnableOrDisableWindowsCopilotForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Remediation-EnableOrDisableWindowsCopilotForCurrentUser.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Enable or Disable Windows Copilot in Windows 11
+# Remediation-EnableOrDisableWindowsCopilotForCurrentUser
+
+$RegPath = 'HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+	$null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'TurnOffWindowsCopilot'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
