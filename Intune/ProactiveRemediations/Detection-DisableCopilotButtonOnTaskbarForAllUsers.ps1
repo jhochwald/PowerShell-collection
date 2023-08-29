5e87a405396cc20c585e9155a0cbc919e908eb09
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:05:00 +0200

    Create Detection-DisableCopilotButtonOnTaskbarForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Detection-DisableCopilotButtonOnTaskbarForAllUsers.ps1
@@ -0,0 +1,26 @@
+﻿#requires -Version 5.0
+
+# Disable Copilot Button on Taskbar for All Users
+# Detection-DisableCopilotButtonOnTaskbarForAllUsers
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer'
+
+try 
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      Exit 1
+   }
+	
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'HideCopilotButton' -ErrorAction SilentlyContinue) -eq 1))
+   {
+      Exit 1
+   }
+}
+catch
+{
+   Exit 1
+}
+
+
+Exit 0
