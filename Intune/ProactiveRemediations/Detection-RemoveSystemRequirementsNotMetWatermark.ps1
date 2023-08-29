dbe5c499aaf6ca9eeb8527037881c412acbf81c6
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 20:28:43 +0200

    Create Detection-RemoveSystemRequirementsNotMetWatermark.ps1

---
+++ Intune/ProactiveRemediations/Detection-RemoveSystemRequirementsNotMetWatermark.ps1
@@ -0,0 +1,32 @@
+﻿#requires -Version 5.0
+
+# Remove "System requirements not met" Watermark on Windows 11 Desktop
+# Detection-RemoveSystemRequirementsNotMetWatermark
+
+$RegPath = 'HKCU:\Control Panel\UnsupportedHardwareNotificationCache'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'SV1' -ErrorAction SilentlyContinue) -eq 0))
+   {
+      exit 1
+   }
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'SV2' -ErrorAction SilentlyContinue) -eq 0))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
