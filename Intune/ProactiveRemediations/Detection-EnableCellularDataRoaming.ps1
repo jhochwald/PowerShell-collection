04ab3f43cf7e57bf8709df4aa9bf052c92ceb1f7
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:11:41 +0200

    Create Detection-EnableCellularDataRoaming.ps1

---
+++ Intune/ProactiveRemediations/Detection-EnableCellularDataRoaming.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 1.0
+
+# Enable Cellular Data Roaming
+# Detection-EnableCellularDataRoaming
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WcmSvc\GroupPolicy'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemProperty -LiteralPath $RegPath -Name 'fBlockRoaming' -ErrorAction SilentlyContinue) -eq $null))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
