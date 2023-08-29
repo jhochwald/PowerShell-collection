2b693ca2f72b524aa2762d030a0f09360c2646b7
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:30 +0200

    Create Remediation-TurnOnPhoneLinkForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Remediation-TurnOnPhoneLinkForCurrentUser.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Turn on Phone Link for current user
+# Remediation-TurnOnPhoneLinkForCurrentUser
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Mobility'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'PhoneLinkEnabled'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
