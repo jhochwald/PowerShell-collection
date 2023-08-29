57ffb2715fc204338af68c3dc05ad14a296cc9e2
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:12 +0200

    Create Remediation-DisablePhoneLinkAppForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Remediation-DisablePhoneLinkAppForAllUsers.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Disable Phone Link app for All Users
+# Remediation-DisablePhoneLinkAppForAllUsers
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'EnableMmx'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
