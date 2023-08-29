e7ba04d625824941f5bb4fd0377a2265adbd0d12
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:41:49 +0200

    Create Detection-EnableOrDisableWindowsCopilotForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Detection-EnableOrDisableWindowsCopilotForCurrentUser.ps1
@@ -0,0 +1,31 @@
+﻿#requires -Version 5.0
+
+# Enable or Disable Windows Copilot in Windows 11
+# Detection-EnableOrDisableWindowsCopilotForCurrentUser
+
+$RegPath = 'HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot'
+
+try 
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      Exit 1
+   }
+
+	
+   if ((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'TurnOffWindowsCopilot' -ErrorAction SilentlyContinue) -eq 0)
+   {
+
+   }
+   else
+   {
+      Exit 1
+   }
+}
+catch
+{
+   Exit 1
+}
+
+
+Exit 0
