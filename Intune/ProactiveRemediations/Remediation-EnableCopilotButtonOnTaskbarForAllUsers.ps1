e187f974af399424f58fcd2bf442d0e02fe905c1
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:05:13 +0200

    Create Remediation-EnableCopilotButtonOnTaskbarForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Remediation-EnableCopilotButtonOnTaskbarForAllUsers.ps1
@@ -0,0 +1,46 @@
+﻿#requires -Version 1.0
+
+# Enable Copilot Button on Taskbar for All Users
+# Remediation-EnableCopilotButtonOnTaskbarForAllUsers
+
+if ((Test-Path -LiteralPath 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer') -ne $true)
+{
+   $paramNewItem = @{
+      Path        = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer'
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+if ((Test-Path -LiteralPath 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideCopilotButton') -ne $true)
+{
+   $paramNewItem = @{
+      Path        = 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideCopilotButton'
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramRemoveItemProperty = @{
+   LiteralPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer'
+   Name        = 'HideCopilotButton'
+   Force       = $true
+   Confirm     = $false
+   ErrorAction = 'SilentlyContinue'
+}
+$null = (Remove-ItemProperty @paramRemoveItemProperty)
+
+$paramNewItemProperty = @{
+   LiteralPath  = 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideCopilotButton'
+   Name         = 'value'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
