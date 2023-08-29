6c232d7882646d77900ce96a2f8292652cb706a6
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:11:39 +0200

    Create Detection-DisableUserChoiceShowAccountEmailAddressOnSignInScreen.ps1

---
+++ Intune/ProactiveRemediations/Detection-DisableUserChoiceShowAccountEmailAddressOnSignInScreen.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 5.0
+
+# Disable User Choice to Show Account Email Address on Sign-in Screen for All Users
+# Detection-DisableUserChoiceShowAccountEmailAddressOnSignInScreen
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'BlockUserFromShowingAccountDetailsOnSignin' -ErrorAction SilentlyContinue) -eq 1))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
