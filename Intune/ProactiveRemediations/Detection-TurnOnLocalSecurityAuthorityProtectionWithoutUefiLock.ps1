59bcf1c754b2ca9683d5244ff0803fe24d91d391
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 19:39:05 +0200

    Create Detection-TurnOnLocalSecurityAuthorityProtectionWithoutUefiLock.ps1

---
+++ Intune/ProactiveRemediations/Detection-TurnOnLocalSecurityAuthorityProtectionWithoutUefiLock.ps1
@@ -0,0 +1,31 @@
+﻿#requires -Version 5.0
+
+# Turn On Local Security Authority (LSA) Protection without UEFI Lock
+# Detection-TurnOnLocalSecurityAuthorityProtectionWithoutUefiLock
+
+$RegPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'RunAsPPL' -ErrorAction SilentlyContinue) -eq 2))
+   {
+      exit 1
+   }
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'RunAsPPLBoot' -ErrorAction SilentlyContinue) -eq 2))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
