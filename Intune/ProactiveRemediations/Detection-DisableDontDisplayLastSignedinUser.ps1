6dd87c6b27f3390b0c845b9bf5f908c6f635a442
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:11:35 +0200

    Create Detection-DisableDontDisplayLastSignedinUser.ps1

---
+++ Intune/ProactiveRemediations/Detection-DisableDontDisplayLastSignedinUser.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 1.0
+
+# Disable "Interactive logon: Don't display last signed-in"
+# Detection-DisableDontDisplayLastSignedinUser
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemProperty -LiteralPath $RegPath -Name 'dontdisplaylastusername' -ErrorAction SilentlyContinue) -eq $null))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
