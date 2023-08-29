0584d181cf431ed018e428b7b28959a307560027
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:24 +0200

    Create Remediation-EnableUserChoiceShowAccountEmailAddressOnSignInScreen.ps1

---
+++ Intune/ProactiveRemediations/Remediation-EnableUserChoiceShowAccountEmailAddressOnSignInScreen.ps1
@@ -0,0 +1,47 @@
+﻿#requires -Version 1.0
+
+# Enable User Choice to Show Account Email Address on Sign-in Screen for All Users
+# Remediation-EnableUserChoiceShowAccountEmailAddressOnSignInScreen
+
+$RegPath2 = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+if ((Test-Path -LiteralPath $RegPath2 -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath2
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramRemoveItemProperty = @{
+   LiteralPath = $RegPath
+   Name        = 'DontDisplayLockedUserId'
+   Force       = $true
+   Confirm     = $false
+   ErrorAction = 'SilentlyContinue'
+}
+$null = (Remove-ItemProperty @paramRemoveItemProperty)
+
+$paramRemoveItemProperty = @{
+   LiteralPath = $RegPath2
+   Name        = 'BlockUserFromShowingAccountDetailsOnSignin'
+   Force       = $true
+   Confirm     = $false
+   ErrorAction = 'SilentlyContinue'
+}
+$null = (Remove-ItemProperty @paramRemoveItemProperty)
