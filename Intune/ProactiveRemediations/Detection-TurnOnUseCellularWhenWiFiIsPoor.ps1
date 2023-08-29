42ca6417fa85b74e42eed8b32c698fe011bca171
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:11:58 +0200

    Create Detection-TurnOnUseCellularWhenWiFiIsPoor.ps1

---
+++ Intune/ProactiveRemediations/Detection-TurnOnUseCellularWhenWiFiIsPoor.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 5.0
+
+# Turn On Use Cellular when Wi-Fi is Poor
+# Detection-TurnOnUseCellularWhenWiFiIsPoor
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\WcmSvc\CellularFailover'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'AllowFailover' -ErrorAction SilentlyContinue) -eq 2))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
