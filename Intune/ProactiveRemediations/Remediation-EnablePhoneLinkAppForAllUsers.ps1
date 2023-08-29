7c1084be8872e891891d05a7f4cb85d15c5d9175
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:22 +0200

    Create Remediation-EnablePhoneLinkAppForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Remediation-EnablePhoneLinkAppForAllUsers.ps1
@@ -0,0 +1,26 @@
+﻿#requires -Version 1.0
+
+# Enable Phone Link app for All Users
+# Remediation-EnablePhoneLinkAppForAllUsers
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramRemoveItemProperty = @{
+   LiteralPath = $RegPath
+   Name        = 'EnableMmx'
+   Force       = $true
+   Confirm     = $false
+   ErrorAction = 'SilentlyContinue'
+}
+$null = (Remove-ItemProperty @paramRemoveItemProperty)
