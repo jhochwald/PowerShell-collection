6bb86434d0dc94fbdff9e2dfede5a7a4a61ccac1
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:11:46 +0200

    Create Detection-EnablePhoneLinkAppForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Detection-EnablePhoneLinkAppForAllUsers.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 1.0
+
+# Enable Phone Link app for All Users
+# Detection-EnablePhoneLinkAppForAllUsers
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemProperty -LiteralPath $RegPath -Name 'EnableMmx' -ErrorAction SilentlyContinue) -eq $null))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
