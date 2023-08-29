0ba1c949d2c30f083c25c9830ad19226d99293ee
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:17 +0200

    Create Remediation-EnableCellularDataRoaming.ps1

---
+++ Intune/ProactiveRemediations/Remediation-EnableCellularDataRoaming.ps1
@@ -0,0 +1,26 @@
+﻿#requires -Version 1.0
+
+# Enable Cellular Data Roaming
+# Remediation-EnableCellularDataRoaming
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WcmSvc\GroupPolicy'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramRemoveItemProperty = @{
+   LiteralPath = $RegPath
+   Name        = 'fBlockRoaming'
+   Force       = $true
+   Confirm     = $false
+   ErrorAction = 'SilentlyContinue'
+}
+$null = (Remove-ItemProperty @paramRemoveItemProperty)
