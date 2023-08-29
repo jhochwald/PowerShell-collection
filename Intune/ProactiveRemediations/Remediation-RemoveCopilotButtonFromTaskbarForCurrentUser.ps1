f355c6d8dd557e8de74243667853a4ea7b958b38
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:41:56 +0200

    Create Remediation-RemoveCopilotButtonFromTaskbarForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Remediation-RemoveCopilotButtonFromTaskbarForCurrentUser.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Remove Copilot Button from Taskbar for current user
+# Remediation-RemoveCopilotButtonFromTaskbarForCurrentUser
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+
+if ((Test-Path -LiteralPath $RegPath) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'ShowCopilotButton'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
