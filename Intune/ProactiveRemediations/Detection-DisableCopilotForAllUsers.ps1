b5397bc33eb2c278fc1ef62eec84141cfec1983c
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:05:03 +0200

    Create Detection-DisableCopilotForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Detection-DisableCopilotForAllUsers.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 5.0
+
+# Disable Copilot for All Users
+# Detection-DisableCopilotForAllUsers
+
+$RegPath = 'HKLM:\Software\Policies\Microsoft\Windows\WindowsCopilot'
+
+try 
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      Exit 1
+   }
+
+	
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'TurnOffWindowsCopilot' -ErrorAction SilentlyContinue) -eq 0))
+   {
+      Exit 1
+   }
+}
+catch
+{
+   Exit 1
+}
+
+
+Exit 0
