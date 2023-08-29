c7fd09641f24436e7f149645238d3741168755b7
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 18:05:11 +0200

    Create Remediation-DisableCopilotForAllUsers.ps1

---
+++ Intune/ProactiveRemediations/Remediation-DisableCopilotForAllUsers.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Disable Copilot for All Users
+# Remediation-DisableCopilotForAllUsers
+
+$RegPath = 'HKLM:\Software\Policies\Microsoft\Windows\WindowsCopilot'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'TurnOffWindowsCopilot'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
