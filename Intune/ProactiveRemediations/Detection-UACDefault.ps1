1e1cbf443b79cf6a5dc94d863b76618201573611
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:03 +0200

    Create Detection-UACDefault.ps1

---
+++ Intune/ProactiveRemediations/Detection-UACDefault.ps1
@@ -0,0 +1,37 @@
+﻿#requires -Version 5.0
+
+# UAC "Notify me only when apps try to make changes to my computer (default)"
+# Detection-UACDefault
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'PromptOnSecureDesktop' -ErrorAction SilentlyContinue) -eq 1))
+   {
+      exit 1
+   }
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'EnableLUA' -ErrorAction SilentlyContinue) -eq 1))
+   {
+      exit 1
+   }
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'ConsentPromptBehaviorAdmin' -ErrorAction SilentlyContinue) -eq 5))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
