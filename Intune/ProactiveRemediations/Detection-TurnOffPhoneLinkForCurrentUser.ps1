867cc51dc820a1b48a8de0a75c8c357cc72f1238
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:11:51 +0200

    Create Detection-TurnOffPhoneLinkForCurrentUser.ps1

---
+++ Intune/ProactiveRemediations/Detection-TurnOffPhoneLinkForCurrentUser.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 5.0
+
+# Turn off Phone Link for current user
+# Detection-TurnOffPhoneLinkForCurrentUser
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Mobility'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'PhoneLinkEnabled' -ErrorAction SilentlyContinue) -eq 0))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+
+exit 0
