6855189da82322bf641e7de603ffe90db4276cca
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:33 +0200

    Create Remediation-TurnOnUseCellularWhenWiFiIsPoor.ps1

---
+++ Intune/ProactiveRemediations/Remediation-TurnOnUseCellularWhenWiFiIsPoor.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Turn On Use Cellular when Wi-Fi is Poor
+# Remediation-TurnOnUseCellularWhenWiFiIsPoor
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\WcmSvc\CellularFailover'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'AllowFailover'
+   Value        = 2
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
