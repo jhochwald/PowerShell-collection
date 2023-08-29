301c76589c8b71b46628156feff77e174f609e98
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 20:18:45 +0200

    Create Remediation-RemoveWindowsWatermarkOnDesktopForAllUser.ps1

---
+++ Intune/ProactiveRemediations/Remediation-RemoveWindowsWatermarkOnDesktopForAllUser.ps1
@@ -0,0 +1,28 @@
+﻿#requires -Version 1.0
+
+# Remove Windows Edition and Build Watermark on Desktop for All Users
+# RemoveWindowsWatermarkOnDesktopForAllUser
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows'
+
+if ((Test-Path -LiteralPath $RegPath) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'DisplayVersion'
+   Value        = 0
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
