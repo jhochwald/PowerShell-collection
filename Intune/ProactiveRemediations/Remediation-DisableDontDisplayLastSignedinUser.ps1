fffc6abe6582be6919e96259faedcf176951806a
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-08-29 23:12:09 +0200

    Create Remediation-DisableDontDisplayLastSignedinUser.ps1

---
+++ Intune/ProactiveRemediations/Remediation-DisableDontDisplayLastSignedinUser.ps1
@@ -0,0 +1,26 @@
+﻿#requires -Version 1.0
+
+# Disable "Interactive logon: Don't display last signed-in"
+# Remediation-DisableDontDisplayLastSignedinUser
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramRemoveItemProperty = @{
+   LiteralPath = $RegPath
+   Name        = 'dontdisplaylastusername'
+   Force       = $true
+   Confirm     = $false
+   ErrorAction = 'SilentlyContinue'
+}
+$null = (Remove-ItemProperty @paramRemoveItemProperty)
