54d2c48a5fe7dd72c301e0124fb2e75859e1271e
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-10-30 20:25:55 +0100

    IsWVDEnvironment

---
+++ Intune/ProactiveRemediations/Detection-IsWVDEnvironment.ps1
@@ -0,0 +1,31 @@
+﻿# Detection-IsWVDEnvironment
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Teams'
+
+try
+{
+   $paramTestPath = @{
+      LiteralPath = $RegPath
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!(Test-Path @paramTestPath))
+   {
+      exit 1
+   }
+
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'IsWVDEnvironment'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 1))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-IsWVDEnvironment.ps1
@@ -0,0 +1,29 @@
+﻿# Remediation-IsWVDEnvironment
+
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Teams'
+
+$paramTestPath = @{
+   LiteralPath = $RegPath
+   ErrorAction = 'SilentlyContinue'
+}
+if ((Test-Path @paramTestPath) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'IsWVDEnvironment'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
