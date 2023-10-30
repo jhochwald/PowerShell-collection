58378ba57af86a9f5262b41f695a86fe4531263c
Joerg Hochwald <jhochwald@users.noreply.github.com>
2023-10-30 20:26:02 +0100

    AllowSideLoadingForTrustedApps

---
+++ Intune/ProactiveRemediations/Detection-AllowSideLoadingForTrustedApps.ps1
@@ -0,0 +1,52 @@
+﻿# Detection-AllowSideLoadingForTrustedApps
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx'
+
+try
+{
+   $paramTestPath = @{
+      LiteralPath = $RegPath
+      ErrorAction = 'SilentlyContinue'
+   }
+
+   if (!(Test-Path @paramTestPath))
+   {
+      exit 1
+   }
+
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'AllowDeploymentInSpecialProfiles'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 1))
+   {
+      exit 1
+   }
+
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'AllowAllTrustedApps'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 1))
+   {
+      exit 1
+   }
+
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'AllowDevelopmentWithoutDevLicense'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 1))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-AllowSideLoadingForTrustedApps.ps1
@@ -0,0 +1,49 @@
+﻿# Remediation-AllowSideLoadingForTrustedApps
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Appx'
+
+$paramTestPath = @{
+   LiteralPath = $RegPath
+   ErrorAction = 'SilentlyContinue'
+}
+if ((Test-Path @paramTestPath) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'AllowDeploymentInSpecialProfiles'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'AllowAllTrustedApps'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'AllowDevelopmentWithoutDevLicense'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
