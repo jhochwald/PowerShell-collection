df61b17b45ee7da32678080dd64f37d902c3e876
Joerg Hochwald <jhochwald@users.noreply.github.com>
2024-06-12 10:11:53 +0200

    Update via upstream

---
+++ Intune/ProactiveRemediations/Detection-VSthroughMUUpdates.ps1
@@ -0,0 +1,35 @@
+﻿# Detection: Opting in to preview Visual Studio updates via Microsoft Update
+# https://developercommunity.visualstudio.com/t/automatic-updates/599126
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\VisualStudio\Setup'
+
+$paramDefaults = @{
+   LiteralPath = $RegPath
+   ErrorAction = 'SilentlyContinue'
+}
+
+try
+{
+   if (!(Test-Path @paramDefaults))
+   {
+      exit 1
+   }
+
+   # Join Preview = 1 / Leave Preview = 0
+   if (!((Get-ItemPropertyValue -Name 'PreviewAutomaticUpdates' @paramDefaults) -eq 1))
+   {
+      exit 1
+   }
+
+   # Opt-in = 0 / Opt-out = 1
+   if (!((Get-ItemPropertyValue -Name 'VSthroughMUUpdatesOptOut' @paramDefaults) -eq 0))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-VSthroughMUUpdates.ps1
@@ -0,0 +1,23 @@
+﻿# Remediation: Opting in to preview Visual Studio updates via Microsoft Update
+# https://developercommunity.visualstudio.com/t/automatic-updates/599126
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\VisualStudio\Setup'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $null = (New-Item -Path $RegPath -Force -Confirm:$false -ErrorAction Stop)
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'Stop'
+}
+
+# Join Preview = 1 / Leave Preview = 0
+$null = (New-ItemProperty -Name 'PreviewAutomaticUpdates' -Value 1 @paramNewItemProperty)
+
+# Opt-in = 0 / Opt-out = 1
+$null = (New-ItemProperty -Name 'VSthroughMUUpdatesOptOut' -Value 0 @paramNewItemProperty)
