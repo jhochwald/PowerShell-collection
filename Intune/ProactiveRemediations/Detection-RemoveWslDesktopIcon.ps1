33e0aba3229e037e40a42324ff628a768c4a9510
Joerg Hochwald <jhochwald@users.noreply.github.com>
2025-05-23 16:43:52 +0200

    Hide/Remove WSL Linux icon from Desktop

---
+++ Intune/ProactiveRemediations/Detection-RemoveWslDesktopIcon.ps1
@@ -0,0 +1,29 @@
+﻿# Hide/Remove WSL Linux icon from Desktop
+# Detection-RemoveWslDesktopIcon
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
+
+try
+{
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      exit 1
+   }
+   
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = '{B2B4A4D1-2754-4140-A2EB-9A76D9D7CDC6}'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue ) -eq 1))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+# Ensure a clean exit!
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-RemoveWslDesktopIcon.ps1
@@ -0,0 +1,37 @@
+﻿# Hide/Remove WSL Linux icon from Desktop
+# Remediation-RemoveWslDesktopIcon
+
+$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
+
+try
+{
+   if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+   {
+      $paramNewItem = @{
+         Path        = $RegPath
+         Force       = $true
+         Confirm     = $false
+         ErrorAction = 'SilentlyContinue'
+      }
+      $null = (New-Item @paramNewItem)
+   }
+   
+   $paramNewItemProperty = @{
+      LiteralPath  = $RegPath
+      Name         = '{B2B4A4D1-2754-4140-A2EB-9A76D9D7CDC6}'
+      Value        = 1
+      PropertyType = 'DWord'
+      Force        = $true
+      Confirm      = $false
+      ErrorAction  = 'SilentlyContinue'
+   }
+   $null = (New-ItemProperty @paramNewItemProperty)
+}
+catch
+{
+   # We should never reach this point!
+   exit 1
+}
+
+# Ensure a clean exit!
+exit 0
