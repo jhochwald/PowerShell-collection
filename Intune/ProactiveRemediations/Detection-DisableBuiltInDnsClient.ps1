935f277160c9f4972111586976a1b6a167908403
Joerg Hochwald <jhochwald@users.noreply.github.com>
2025-05-23 16:43:40 +0200

    Disable Browser built in DNS client for full Global Secure Access (GSA) support

---
+++ Intune/ProactiveRemediations/Detection-DisableBuiltInDnsClient.ps1
@@ -0,0 +1,36 @@
+﻿# Disable Browser built in DNS client for full Global Secure Access (GSA) support
+# Detection-DisableBuiltInDnsClient
+
+$AllRegpath = @(
+   'HKLM:\SOFTWARE\Policies\BraveSoftware\Brave'
+   'HKLM:\SOFTWARE\Policies\Google\Chrome'
+   'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+)
+
+try
+{
+   foreach ($RegPath in $AllRegpath)
+   {
+      if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+      {
+exit 1
+}
+      
+      $paramGetItemPropertyValue = @{
+         LiteralPath = $RegPath
+         Name        = 'BuiltInDnsClientEnabled '
+         ErrorAction = 'SilentlyContinue'
+      }
+      if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 0))
+      {
+exit 1
+}
+   }
+}
+catch
+{
+exit 1
+}
+
+# Ensure a clean exit!
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-DisableBuiltInDnsClient.ps1
@@ -0,0 +1,44 @@
+﻿# Disable Browser built in DNS client for full Global Secure Access (GSA) support
+# Remediation-DisableBuiltInDnsClient
+
+$AllRegpath = @(
+   'HKLM:\SOFTWARE\Policies\BraveSoftware\Brave'
+   'HKLM:\SOFTWARE\Policies\Google\Chrome'
+   'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+)
+
+try
+{
+   foreach ($RegPath in $AllRegpath)
+   {
+      if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+      {
+         $paramNewItem = @{
+            Path        = $RegPath
+            Force       = $true
+            Confirm     = $false
+            ErrorAction = 'SilentlyContinue'
+         }
+         $null = (New-Item @paramNewItem)
+      }
+      
+      $paramNewItemProperty = @{
+         LiteralPath  = $RegPath
+         Name         = 'BuiltInDnsClientEnabled'
+         Value        = 0
+         PropertyType = 'DWord'
+         Force        = $true
+         Confirm      = $false
+         ErrorAction  = 'SilentlyContinue'
+      }
+      $null = (New-ItemProperty @paramNewItemProperty)
+   }
+}
+catch
+{
+   # We should never reach this point!
+   exit 1
+}
+
+# Ensure a clean exit!
+exit 0
