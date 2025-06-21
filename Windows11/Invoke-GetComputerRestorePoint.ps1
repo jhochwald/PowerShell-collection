5ea2cbbc304b5791a0446188ee18f72090497a9f
Joerg Hochwald <jhochwald@users.noreply.github.com>
2025-06-22 00:34:47 +0200

    Get the existing restore points and show only the required info.

---
+++ Windows11/Invoke-GetComputerRestorePoint.ps1
@@ -0,0 +1,30 @@
+﻿#requires -Version 2.0 -RunAsAdministrator
+
+<#
+      .SYNOPSIS
+      Get the existing restore points
+
+      .DESCRIPTION
+      Get the existing restore points and show only the required info.
+
+      .EXAMPLE
+      PS C:\> .\Invoke-GetComputerRestorePoint.ps1
+
+      Get the existing restore points and show only the required info.
+
+      .NOTES
+      The Date is transformed into a human radable form using the ManagementDateTimeConverter.ToDateTime(String) method
+#>
+[CmdletBinding(ConfirmImpact = 'None')]
+[OutputType([array])]
+param ()
+
+process
+{
+   Get-ComputerRestorePoint -ErrorAction Stop | Select-Object -Property @{
+      n = 'Date'
+      e = {
+         [Management.ManagementDateTimeConverter]::ToDateTime($_.creationtime)
+      }
+   }, Description, SequenceNumber | Sort-Object -Property Date
+}
