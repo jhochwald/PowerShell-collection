433e401d5a64a1673ce6aaf096f5e5406dda3a2a
Joerg Hochwald <jhochwald@users.noreply.github.com>
2025-10-15 16:43:15 +0200

    Create Invoke-SpeedtestViaCli.ps1

---
+++ Misc/Invoke-SpeedtestViaCli.ps1
@@ -0,0 +1,25 @@
+﻿#requires -Version 3.0
+
+# Execute speed test via the CLI
+$Speedtest = (& 'C:\Tools\Speedtest\speedtest.exe' --progress=no --format=json --accept-license --accept-gdpr | ConvertFrom-Json)
+
+# Create New Object SpeedObject
+[PSCustomObject]$SpeedObject = [ordered] @{
+   downloadspeed = [math]::Round($Speedtest.download.bandwidth / 1000000 * 8, 2)
+   uploadspeed   = [math]::Round($Speedtest.upload.bandwidth / 1000000 * 8, 2)
+   packetloss    = [math]::Round($Speedtest.packetLoss)
+   Latency       = [math]::Round($Speedtest.ping.latency)
+   isp           = $Speedtest.isp
+   ExternalIP    = $Speedtest.interface.externalIp
+   InternalIP    = $Speedtest.interface.internalIp
+   UsedServer    = $Speedtest.server.host
+   URL           = $Speedtest.result.url
+   Jitter        = [math]::Round($Speedtest.ping.jitter)    
+}
+
+# Dump it
+$SpeedObject
+
+# Cleanup
+$Speedtest = $null
+$SpeedObject = $null
