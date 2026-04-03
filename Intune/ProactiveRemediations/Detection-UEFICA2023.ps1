be9b2e1782ccecad1e7d36a0ff9a5ea65f141c2a
Joerg Hochwald <jhochwald@users.noreply.github.com>
2026-04-03 22:50:41 +0200

    Secure Boot 2023 certificate remediation

---
+++ Intune/ProactiveRemediations/Detection-UEFICA2023.ps1
@@ -0,0 +1,34 @@
+﻿#requires -Version 2.0 -Modules SecureBoot -RunAsAdministrator
+
+<#
+      Secure Boot 2023 certificate remediation
+      Detection-UEFICA2023
+#>
+
+try
+{
+   $paramGetSecureBootUEFI = @{
+      Name          = 'db'
+      ErrorAction   = 'Stop'
+      WarningAction = 'SilentlyContinue'
+   }
+   if ([Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI @paramGetSecureBootUEFI).Bytes) -match 'UEFI CA 2023')
+   {
+      Write-Host -Object 'UEFI CA 2023 found'
+      
+      exit 0
+   }
+   else
+   {
+      Write-Host -Object 'UEFI CA 2023 NOT found'
+      
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+# Default
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-UEFICA2023.ps1
@@ -0,0 +1,51 @@
+﻿#requires -Version 2.0 -Modules ScheduledTasks, SecureBoot -RunAsAdministrator
+
+<#
+      Secure Boot 2023 certificate remediation
+      Remediation-UEFICA2023
+#>
+
+try
+{
+   $paramGetSecureBootUEFI = @{
+      Name          = 'db'
+      ErrorAction   = 'Stop'
+      WarningAction = 'SilentlyContinue'
+   }
+   if (!([Text.Encoding]::ASCII.GetString((Get-SecureBootUEFI @paramGetSecureBootUEFI).Bytes) -match 'UEFI CA 2023'))
+   {
+      # Remediation kick in
+      $paramNewItemProperty = @{
+         Path          = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot'
+         Name          = 'AvailableUpdates'
+         Value         = 0x5944
+         PropertyType  = 'DWord'
+         Force         = $true
+         Confirm       = $false
+         ErrorAction   = 'Stop'
+         WarningAction = 'SilentlyContinue'
+      }
+      $null = (New-ItemProperty @paramNewItemProperty)
+      $paramNewItemProperty = $null
+      
+      $paramScheduledTask = @{
+         TaskName      = 'Secure-Boot-Update'
+         TaskPath      = '\Microsoft\Windows\PI\'
+         WarningAction = 'SilentlyContinue'
+      }
+      
+      if (Get-ScheduledTask @paramScheduledTask -ErrorAction SilentlyContinue)
+      {
+         $null = (Start-ScheduledTask @paramScheduledTask -ErrorAction Stop)
+      }
+      
+      exit 0
+   }
+}
+catch
+{
+   exit 1
+}
+
+# Default
+exit 0
