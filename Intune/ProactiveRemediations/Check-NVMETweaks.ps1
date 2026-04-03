8cfa1ae1d7afd460ad4953215e4d327e0b7b5c1f
Joerg Hochwald <jhochwald@users.noreply.github.com>
2026-04-03 22:50:29 +0200

    Some relatively aggressive NVME tweaks for Windows 11

--- Intune/ProactiveRemediations/Check-NVMETweaks.ps1
+++ Intune/ProactiveRemediations/Check-NVMETweaks.ps1
@@ -1,4 +1,4 @@
-﻿# Check: Some relatively aggresiv NVME tweaks for Windows 11
+# Check: Some relatively aggressive NVME tweaks for Windows 11
 # Check-NVMETweaks.ps1
 
 try
@@ -11,79 +11,31 @@ try
       exit 1
    }
 
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'QueueDepth' -ErrorAction SilentlyContinue) -eq 64))
-   {
-      exit 1
+   $expectedValues = @{
+      'QueueDepth'              = 64
+      'NvmeMaxReadSplit'        = 4
+      'NvmeMaxWriteSplit'       = 4
+      'ForceFlush'              = 1
+      'ImmediateData'           = 1
+      'MaxSegmentsPerCommand'   = 256
+      'MaxOutstandingCmds'      = 256
+      'ForceEagerWrites'        = 1
+      'MaxQueuedCommands'       = 256
+      'MaxOutstandingIORequests'= 256
+      'NumberOfRequests'        = 1500
+      'IoSubmissionQueueCount'  = 3
+      'IoQueueDepth'            = 64
+      'HostMemoryBufferBytes'   = 1500
+      'ArbitrationBurst'        = 256
    }
 
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'NvmeMaxReadSplit' -ErrorAction SilentlyContinue) -eq 4))
+   foreach ($name in $expectedValues.Keys)
    {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'NvmeMaxWriteSplit' -ErrorAction SilentlyContinue) -eq 4))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'ForceFlush' -ErrorAction SilentlyContinue) -eq 1))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'ImmediateData' -ErrorAction SilentlyContinue) -eq 1))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'MaxSegmentsPerCommand' -ErrorAction SilentlyContinue) -eq 256))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'MaxOutstandingCmds' -ErrorAction SilentlyContinue) -eq 256))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'ForceEagerWrites' -ErrorAction SilentlyContinue) -eq 1))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'MaxQueuedCommands' -ErrorAction SilentlyContinue) -eq 256))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'MaxOutstandingIORequests' -ErrorAction SilentlyContinue) -eq 256))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'NumberOfRequests' -ErrorAction SilentlyContinue) -eq 1500))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'IoSubmissionQueueCount' -ErrorAction SilentlyContinue) -eq 3))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'IoQueueDepth' -ErrorAction SilentlyContinue) -eq 64))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'HostMemoryBufferBytes' -ErrorAction SilentlyContinue) -eq 1500))
-   {
-      exit 1
-   }
-
-   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name 'ArbitrationBurst' -ErrorAction SilentlyContinue) -eq 256))
-   {
-      exit 1
+      $value = Get-ItemPropertyValue -LiteralPath $RegPath -Name $name -ErrorAction SilentlyContinue
+      if ($value -ne $expectedValues[$name])
+      {
+         exit 1
+      }
    }
    #endregion ServicesstornvmeParameters
 
