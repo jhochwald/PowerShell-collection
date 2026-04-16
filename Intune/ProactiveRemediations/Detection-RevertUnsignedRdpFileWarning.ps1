50867cb26e1f06c13779bc861bc6b39cf820f897
Joerg Hochwald <jhochwald@users.noreply.github.com>
2026-04-16 18:49:43 +0200

    Revert Unsigned RDP File Warning (Remote Desktop Connection security warning)

---
+++ Intune/ProactiveRemediations/Detection-RevertUnsignedRdpFileWarning.ps1
@@ -0,0 +1,43 @@
+﻿# Check: Revert Unsigned RDP File Warning (Remote Desktop Connection security warning)
+# Detection-RevertUnsignedRdpFileWarning.ps1
+
+<#
+      If you get the following warning (introduced with Windows Updates) and want to revert to the old behavior:
+      Caution: Unknown remote connection
+
+      This remote connection could harm the local or remote computer and may be used to steal passwords or files.
+      We could not verify the publisher of this remote connection. Stop now unless youare certain you trust this connection.
+      Contact your IT department if unsure, Learn more.
+#>
+
+
+try
+{
+   $RegPath = 'HKLM:\Software\Policies\Microsoft\Windows NT\Terminal Services\Client'
+   $paramTestPath = @{
+      LiteralPath = $RegPath
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!(Test-Path @paramTestPath))
+   {
+      exit 1
+   }
+   $paramTestPath = $null
+
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'RedirectionWarningDialogVersion'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 1))
+   {
+      exit 1
+   }
+   $paramGetItemPropertyValue = $null
+}
+catch
+{
+   exit 1
+}
+
+exit 0
---
+++ Intune/ProactiveRemediations/Remediation-RevertUnsignedRdpFileWarning.ps1
@@ -0,0 +1,37 @@
+﻿# Remediate: Revert Unsigned RDP File Warning (Remote Desktop Connection security warning)
+# Remediation-RevertUnsignedRdpFileWarning.ps1
+
+<#
+      If you get the following warning (introduced with Windows Updates) and want to revert to the old behavior:
+      Caution: Unknown remote connection
+
+      This remote connection could harm the local or remote computer and may be used to steal passwords or files.
+      We could not verify the publisher of this remote connection. Stop now unless youare certain you trust this connection.
+      Contact your IT department if unsure, Learn more.
+#>
+
+$RegPath = 'HKLM:\Software\Policies\Microsoft\Windows NT\Terminal Services\Client'
+
+if ((Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue) -ne $true)
+{
+   $paramNewItem = @{
+      Path        = $RegPath
+      Force       = $true
+      Confirm     = $false
+      ErrorAction = 'SilentlyContinue'
+   }
+   $null = (New-Item @paramNewItem)
+   $paramNewItem = $null
+}
+
+$paramNewItemProperty = @{
+   LiteralPath  = $RegPath
+   Name         = 'RedirectionWarningDialogVersion'
+   Value        = 1
+   PropertyType = 'DWord'
+   Force        = $true
+   Confirm      = $false
+   ErrorAction  = 'SilentlyContinue'
+}
+$null = (New-ItemProperty @paramNewItemProperty)
+$paramNewItemProperty = $null
